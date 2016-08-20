d43e5e19bd91f8992b4aafdc67ec346461968e1f
GoGo228 <ghhfghhf5@gmail.com>
2016-08-20 21:12:59 +0300

    hi

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\SUPER\Documents\Visual Studio 2015\Projects\ConsoleApplication5\ConsoleApplication5\note.txt";
+            Serialize s = new Serialize();
+            
+            try
+            {
+                
+                var a = Convert.ToDouble(Console.ReadLine());
+                s.NewData(path,a);
+                s.EndData(path);
+            }
+            catch (Exception e)
+            {
+                s.ExeptionData(path, e.Message);
+            }
+
+        }
+    }
+}
---
+++ SerializeModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    [Serializable]
+    class SerializeModel
+    {
+        
+        
+            public double Number2 { get; set; }
+            public double Number1 { get; set; }
+            public char Operation { get; set; }
+            public double Answer { get; set; }
+
+            public SerializeModel(double number1, double number2, char operation, double answer)
+            {
+                Number1 = number1;
+                Number2 = number2;
+                Operation = operation;
+                Answer = answer;
+
+            }
+        }
+    }
