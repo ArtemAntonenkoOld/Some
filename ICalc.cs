840aa915ec10c7b2de6a5dfa0115116e5d70d237
Meelistenso <meelistenso@gmail.com>
2016-08-22 12:37:44 +0300

    Начинаем

---
+++ ICalc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    public interface ICalc<T>
+    {
+        Result<T> Summ(T val);
+        string poslat();
+    }
+}
