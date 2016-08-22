d79529a2f1c84957aac225ef57b4e36081f68fe9
Meelistenso <meelistenso@gmail.com>
2016-08-22 19:26:17 +0300

    Заработало

---
+++ Serialize.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    public class Serialize
+    {
+        public void NewData(string path,double a)
+        {
+            using (StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default))
+            {
+                
+                
+                sw.Write(a);
+            }
+        }
+        public void ExeptionData(string path, string a)
+        {
+            using (StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default))
+            {
+
+
+                sw.WriteLine(a);
+            }
+        }
+        public void EndData(string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default))
+            {
+
+
+                sw.WriteLine();
+            }
+        }
+
+
+    }
+
+}
---
+++ SerializeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    [Serializable]
+    class SerializeModel
+    {
+
+            public double Number2 { get; set; }
+            public double Number1 { get; set; }
+            public char Operation { get; set; }
+            public double Answer { get; set; }
+        }
+    }
