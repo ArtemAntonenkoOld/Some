ffb1f6b6c7a9a5470a6a617495f531f43127331a
Meelistenso <meelistenso@gmail.com>
2016-08-22 12:49:30 +0300

    Пытаюсь напилить даубл

---
+++ DoubleCalc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class DoubleCalc : ICalc<double>
+    {
+        public Result<double> Summ(double val1, double val2)
+        {
+            Result<double> res = new Result<double>();
+            try
+            {
+                res.val = val1 + val2;
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+                break;
+            }
+            return res;
+            }
+    }
+}
